087f3c0c2f7eddff09625168dadab9393dff9224
roshnik1 <roshnikhatri87@gmail.com>
2024-10-11 23:29:18 -0600

    Fix build dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-module.exports = {
-    basePath: '/e-portfolio',
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    basePath: '/e-portfolio', // Replace with your repository name if different
     trailingSlash: true,
-  };
+};
+
+module.exports = nextConfig;
