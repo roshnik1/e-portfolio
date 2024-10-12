2c52de5da933eb617bdc418eb125403314f0aa5c
roshnik1 <roshnikhatri87@gmail.com>
2024-10-12 17:08:44 -0600

    Fix resume link

--- components/ExpCards.tsx
+++ components/ExpCards.tsx
@@ -129,7 +129,7 @@ export default function ExpCard() {
       </>
       <div className="lg:px-12 mt-12">
         <a
-          href="mailto:alexander@meikopoulos.com"
+          href="/Resume Draft.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center font-medium leading-tight text-foreground group"
