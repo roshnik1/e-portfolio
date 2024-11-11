5f372d7c43296b82999350bb73ecd8d0651c8800
roshnik1 <roshnikhatri87@gmail.com>
2024-11-11 14:48:45 -0700

    Fix sizing issue

--- components/About.tsx
+++ components/About.tsx
@@ -16,25 +16,25 @@ export default function About() {
         </h2>
       </div>
       <div className="flex flex-col gap-4">
-        <p className="text-start text-muted-foreground lg:px-2 text-[17px] w-[550px]">
+        <p className="text-start text-muted-foreground lg:px-2">
         I’m a data analyst with a passion for turning complex datasets into actionable insights.
         Back in college, I discovered my passion for <BoldT>data analytics</BoldT> through 
         various internships and projects, where I worked on projects processing large-scale retail and financial
         data, sparking my interest in using data to solve real-world problems.
         </p>
-        <p className="text-start  text-muted-foreground lg:px-2 text-[17px] w-[550px]">
+        <p className="text-start  text-muted-foreground lg:px-2">
         Currently, I am a SAP Business Analyst at a global corporation, specializing in 
         <BoldT> Power BI, SQL,</BoldT> and <BoldT>Python</BoldT> to help teams 
         make data-driven decisions and drive business growth. I build dashboards, analyze trends, 
         and uncover insights that empower teams to make informed choices 
         and improve performance.
         </p>
-        <p className="text-start text-muted-foreground lg:px-2 text-[17px] w-[550px]">
+        <p className="text-start text-muted-foreground lg:px-2">
         In previous roles, I’ve worked across industries like <BoldT> healthcare </BoldT>and <BoldT> finance, </BoldT>
         where I developed business intelligence solutions, automated data processes, 
         and collaborated with teams to ensure key decision-makers had access to the insights they needed.
         </p>
-        <p className="text-start text-muted-foreground lg:px-2 text-[17px] w-[550px]">
+        <p className="text-start text-muted-foreground lg:px-2">
         Outside of work, I’m an outdoor enthusiast, hiking and trail running through Colorado’s mountains. 
         I also love reading and learning, continually striving for personal and professional growth. 
         </p>
