fcc30361eb675b9ec50030513f753bc86b8a2746
roshnik1 <roshnikhatri87@gmail.com>
2024-11-13 20:47:17 -0700

    New section titles

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -74,6 +74,11 @@ const jobProjects = [
 export default function Projects() {
   return (
     <section id="projects" className="scroll-mt-16 lg:mt-16">
+      <h2 className="text-base lg:text-start text-bold">
+        E X P E R I E N C E
+        <br />
+        <br />
+      </h2>
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background/0 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
           Projects
